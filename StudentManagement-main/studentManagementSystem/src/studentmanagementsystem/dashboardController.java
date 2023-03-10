e44fe8bd8a23db82d196bda3f84cb2292352c4fe
Tepinsim <thydateppin@yahoo.com>
2023-03-10 23:36:38 +0700

    dashboard

---
+++ StudentManagement-main/studentManagementSystem/src/studentmanagementsystem/dashboardController.java
@@ -0,0 +1,418 @@
+package studentmanagementsystem;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class dashboardController implements Initializable {
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private Button close;
+
+    @FXML
+    private Button minimize;
+
+    @FXML
+    private Label username;
+
+    @FXML
+    private Button home_btn;
+
+    @FXML
+    private Button addStudents_btn;
+
+    @FXML
+    private Button availableCourse_btn;
+
+    @FXML
+    private Button studentGrade_btn;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private AnchorPane home_form;
+
+    @FXML
+    private Label home_totalEnrolled;
+
+    @FXML
+    private Label home_totalFemale;
+
+    @FXML
+    private Label home_totalMale;
+
+    @FXML
+    private BarChart<?, ?> home_totalEnrolledChart;
+
+    @FXML
+    private AreaChart<?, ?> home_totalFemaleChart;
+
+    @FXML
+    private LineChart<?, ?> home_totalMaleChart;
+
+    @FXML
+    private AnchorPane addStudents_form;
+
+    @FXML
+    private TextField addStudents_search;
+
+    @FXML
+    private TableView<studentData> addStudents_tableView;
+
+    @FXML
+    private TableColumn<studentData, String> addStudents_col_studentNum;
+
+    @FXML
+    private TableColumn<studentData, String> addStudents_col_year;
+
+    @FXML
+    private TableColumn<studentData, String> addStudents_col_course;
+
+    @FXML
+    private TableColumn<studentData, String> addStudents_col_firstName;
+
+    @FXML
+    private TableColumn<studentData, String> addStudents_col_lastName;
+
+    @FXML
+    private TableColumn<studentData, String> addStudents_col_gender;
+
+    @FXML
+    private TableColumn<studentData, String> addStudents_col_birth;
+
+    @FXML
+    private TableColumn<studentData, String> addStudents_col_status;
+
+    @FXML
+    private TextField addStudents_studentNum;
+
+    @FXML
+    private ComboBox<?> addStudents_year;
+
+    @FXML
+    private ComboBox<?> addStudents_course;
+
+    @FXML
+    private TextField addStudents_firstName;
+
+    @FXML
+    private TextField addStudents_lastName;
+
+    @FXML
+    private DatePicker addStudents_birth;
+
+    @FXML
+    private ComboBox<?> addStudents_status;
+
+    @FXML
+    private ComboBox<?> addStudents_gender;
+
+    @FXML
+    private ImageView addStudents_imageView;
+
+    @FXML
+    private Button addStudents_insertBtn;
+
+    @FXML
+    private Button addStudents_addBtn;
+
+    @FXML
+    private Button addStudents_updateBtn;
+
+    @FXML
+    private Button addStudents_deleteBtn;
+
+    @FXML
+    private Button addStudents_clearBtn;
+
+    @FXML
+    private AnchorPane availableCourse_form;
+
+    @FXML
+    private TextField availableCourse_course;
+
+    @FXML
+    private TextField availableCourse_description;
+
+    @FXML
+    private TextField availableCourse_degree;
+
+    @FXML
+    private Button availableCourse_addBtn;
+
+    @FXML
+    private Button availableCourse_updateBtn;
+
+    @FXML
+    private Button availableCourse_clearBtn;
+
+    @FXML
+    private Button availableCourse_deleteBtn;
+
+    @FXML
+    private TableView<courseData> availableCourse_tableView;
+
+    @FXML
+    private TableColumn<courseData, String> availableCourse_col_course;
+
+    @FXML
+    private TableColumn<courseData, String> availableCourse_col_description;
+
+    @FXML
+    private TableColumn<courseData, String> availableCourse_col_degree;
+
+    @FXML
+    private AnchorPane studentGrade_form;
+
+    @FXML
+    private TextField studentGrade_studentNum;
+
+    @FXML
+    private Label studentGrade_year;
+
+    @FXML
+    private Label studentGrade_course;
+
+    @FXML
+    private TextField studentGrade_firstSem;
+
+    @FXML
+    private TextField studentGrade_secondSem;
+
+    @FXML
+    private Button studentGrade_updateBtn;
+
+    @FXML
+    private Button studentGrade_clearBtn;
+
+    @FXML
+    private TableView<studentData> studentGrade_tableView;
+
+    @FXML
+    private TableColumn<studentData, String> studentGrade_col_studentNum;
+
+    @FXML
+    private TableColumn<studentData, String> studentGrade_col_year;
+
+    @FXML
+    private TableColumn<studentData, String> studentGrade_col_course;
+
+    @FXML
+    private TableColumn<studentData, String> studentGrade_col_firstSem;
+
+    @FXML
+    private TableColumn<studentData, String> studentGrade_col_secondSem;
+
+    @FXML
+    private TableColumn<studentData, String> studentGrade_col_final;
+
+    @FXML
+    private TextField studentGrade_search;
+
+    private Connection connect;
+    private PreparedStatement prepare;
+    private Statement statement;
+    private ResultSet result;
+
+    private Image image;
+
+    public void homeDisplayTotalEnrolledStudents() {
+
+        String sql = "SELECT COUNT(id) FROM student";
+
+        connect = database.connectDb();
+
+        int countEnrolled = 0;
+
+        try {
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            if (result.next()) {
+                countEnrolled = result.getInt("COUNT(id)");
+            }
+
+            home_totalEnrolled.setText(String.valueOf(countEnrolled));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void homeDisplayFemaleEnrolled() {
+
+        String sql = "SELECT COUNT(id) FROM student WHERE gender = 'Female' and status = 'Enrolled'";
+
+        connect = database.connectDb();
+
+        try {
+            int countFemale = 0;
+
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            if (result.next()) {
+                countFemale = result.getInt("COUNT(id)");
+            }
+
+            home_totalFemale.setText(String.valueOf(countFemale));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void homeDisplayMaleEnrolled() {
+
+        String sql = "SELECT COUNT(id) FROM student WHERE gender = 'Male' and status = 'Enrolled'";
+
+        connect = database.connectDb();
+
+        try {
+            int countMale = 0;
+
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            if (result.next()) {
+                countMale = result.getInt("COUNT(id)");
+            }
+            home_totalMale.setText(String.valueOf(countMale));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void homeDisplayTotalEnrolledChart() {
+
+        home_totalEnrolledChart.getData().clear();
+
+        String sql = "SELECT date, COUNT(id) FROM student WHERE status = 'Enrolled' GROUP BY date ORDER BY TIMESTAMP(date) ASC LIMIT 5";
+
+        connect = database.connectDb();
+
+        try {
+            XYChart.Series chart = new XYChart.Series();
+
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            while (result.next()) {
+                chart.getData().add(new XYChart.Data(result.getString(1), result.getInt(2)));
+            }
+
+            home_totalEnrolledChart.getData().add(chart);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void homeDisplayFemaleEnrolledChart() {
+
+        home_totalFemaleChart.getData().clear();
+
+        String sql = "SELECT date, COUNT(id) FROM student WHERE status = 'Enrolled' and gender = 'Female' GROUP BY date ORDER BY TIMESTAMP(date) ASC LIMIT 5";
+
+        connect = database.connectDb();
+
+        try {
+            XYChart.Series chart = new XYChart.Series();
+
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            while (result.next()) {
+                chart.getData().add(new XYChart.Data(result.getString(1), result.getInt(2)));
+            }
+
+            home_totalFemaleChart.getData().add(chart);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void homeDisplayEnrolledMaleChart() {
+
+        home_totalMaleChart.getData().clear();
+
+        String sql = "SELECT date, COUNT(id) FROM student WHERE status = 'Enrolled' and gender = 'Male' GROUP BY date ORDER BY TIMESTAMP(date) ASC LIMIT 5";
+
+        connect = database.connectDb();
+
+        try {
+            XYChart.Series chart = new XYChart.Series();
+
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            while (result.next()) {
+                chart.getData().add(new XYChart.Data(result.getString(1), result.getInt(2)));
+            }
+
+            home_totalMaleChart.getData().add(chart);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
