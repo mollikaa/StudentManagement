d50d6cb6b09d1aeda04bb2b540880190797c18a3
QxxQ-02 <93064098+QxxQ-02@users.noreply.github.com>
2023-03-11 10:10:37 +0700

    Update dashboardController.java

--- StudentManagement-main/studentManagementSystem/src/studentmanagementsystem/dashboardController.java
+++ StudentManagement-main/studentManagementSystem/src/studentmanagementsystem/dashboardController.java
@@ -411,8 +411,447 @@ public class dashboardController implements Initializable {
 
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void addStudentsAdd() {
+
+        String insertData = "INSERT INTO student "
+                + "(studentNum,year,course,firstName,lastName,gender,birth,status,image,date) "
+                + "VALUES(?,?,?,?,?,?,?,?,?,?)";
+
+        connect = database.connectDb();
+
+        try {
+            Alert alert;
+
+            if (addStudents_studentNum.getText().isEmpty()
+                    || addStudents_year.getSelectionModel().getSelectedItem() == null
+                    || addStudents_course.getSelectionModel().getSelectedItem() == null
+                    || addStudents_firstName.getText().isEmpty()
+                    || addStudents_lastName.getText().isEmpty()
+                    || addStudents_gender.getSelectionModel().getSelectedItem() == null
+                    || addStudents_birth.getValue() == null
+                    || addStudents_status.getSelectionModel().getSelectedItem() == null
+                    || getData.path == null || getData.path == "") {
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all blank fields");
+                alert.showAndWait();
+            } else {
+                String checkData = "SELECT studentNum FROM student WHERE studentNum = '"
+                        + addStudents_studentNum.getText() + "'";
+
+                statement = connect.createStatement();
+                result = statement.executeQuery(checkData);
+
+                if (result.next()) {
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Student #" + addStudents_studentNum.getText() + " was already exist!");
+                    alert.showAndWait();
+                } else {
+                    prepare = connect.prepareStatement(insertData);
+                    prepare.setString(1, addStudents_studentNum.getText());
+                    prepare.setString(2, (String) addStudents_year.getSelectionModel().getSelectedItem());
+                    prepare.setString(3, (String) addStudents_course.getSelectionModel().getSelectedItem());
+                    prepare.setString(4, addStudents_firstName.getText());
+                    prepare.setString(5, addStudents_lastName.getText());
+                    prepare.setString(6, (String) addStudents_gender.getSelectionModel().getSelectedItem());
+                    prepare.setString(7, String.valueOf(addStudents_birth.getValue()));
+                    prepare.setString(8, (String) addStudents_status.getSelectionModel().getSelectedItem());
+
+                    String uri = getData.path;
+                    uri = uri.replace("\\", "\\\\");
+                    prepare.setString(9, uri);
+
+                    Date date = new Date();
+                    java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+                    prepare.setString(10, String.valueOf(sqlDate));
+
+                    prepare.executeUpdate();
+
+                    String insertStudentGrade = "INSERT INTO student_grade "
+                            + "(studentNum,year,course,first_sem,second_sem,final) "
+                            + "VALUES(?,?,?,?,?,?)";
+
+                    prepare = connect.prepareStatement(insertStudentGrade);
+                    prepare.setString(1, addStudents_studentNum.getText());
+                    prepare.setString(2, (String) addStudents_year.getSelectionModel().getSelectedItem());
+                    prepare.setString(3, (String) addStudents_course.getSelectionModel().getSelectedItem());
+                    prepare.setString(4, "0");
+                    prepare.setString(5, "0");
+                    prepare.setString(6, "0");
+
+                    prepare.executeUpdate();
+
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Added!");
+                    alert.showAndWait();
+
+                    addStudentsShowListData();
+
+                    addStudentsClear();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addStudentsUpdate() {
+
+        String uri = getData.path;
+        uri = uri.replace("\\", "\\\\");
+
+        String updateData = "UPDATE student SET "
+                + "year = '" + addStudents_year.getSelectionModel().getSelectedItem()
+                + "', course = '" + addStudents_course.getSelectionModel().getSelectedItem()
+                + "', firstName = '" + addStudents_firstName.getText()
+                + "', lastName = '" + addStudents_lastName.getText()
+                + "', gender = '" + addStudents_gender.getSelectionModel().getSelectedItem()
+                + "', birth = '" + addStudents_birth.getValue()
+                + "', status = '" + addStudents_status.getSelectionModel().getSelectedItem()
+                + "', image = '" + uri + "' WHERE studentNum = '"
+                + addStudents_studentNum.getText() + "'";
+
+        connect = database.connectDb();
+
+        try {
+            Alert alert;
+            if (addStudents_studentNum.getText().isEmpty()
+                    || addStudents_year.getSelectionModel().getSelectedItem() == null
+                    || addStudents_course.getSelectionModel().getSelectedItem() == null
+                    || addStudents_firstName.getText().isEmpty()
+                    || addStudents_lastName.getText().isEmpty()
+                    || addStudents_gender.getSelectionModel().getSelectedItem() == null
+                    || addStudents_birth.getValue() == null
+                    || addStudents_status.getSelectionModel().getSelectedItem() == null
+                    || getData.path == null || getData.path == "") {
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all blank fields");
+                alert.showAndWait();
+            } else {
+
+                alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Confirmation Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Are you sure you want to UPDATE Student #" + addStudents_studentNum.getText() + "?");
+                Optional<ButtonType> option = alert.showAndWait();
+
+                if (option.get().equals(ButtonType.OK)) {
+                    statement = connect.createStatement();
+                    statement.executeUpdate(updateData);
+
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Updated!");
+                    alert.showAndWait();
+
+                    addStudentsShowListData();
+
+                    addStudentsClear();
+
+                } else {
+                    return;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addStudentsDelete() {
+
+        String deleteData = "DELETE FROM student WHERE studentNum = '"
+                + addStudents_studentNum.getText() + "'";
+
+        connect = database.connectDb();
+
+        try {
+            Alert alert;
+            if (addStudents_studentNum.getText().isEmpty()
+                    || addStudents_year.getSelectionModel().getSelectedItem() == null
+                    || addStudents_course.getSelectionModel().getSelectedItem() == null
+                    || addStudents_firstName.getText().isEmpty()
+                    || addStudents_lastName.getText().isEmpty()
+                    || addStudents_gender.getSelectionModel().getSelectedItem() == null
+                    || addStudents_birth.getValue() == null
+                    || addStudents_status.getSelectionModel().getSelectedItem() == null
+                    || getData.path == null || getData.path == "") {
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all blank fields");
+                alert.showAndWait();
+            } else {
+                alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Confirmation Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Are you sure you want to DELETE Student #" + addStudents_studentNum.getText() + "?");
+
+                Optional<ButtonType> option = alert.showAndWait();
+
+                if (option.get().equals(ButtonType.OK)) {
+
+                    statement = connect.createStatement();
+                    statement.executeUpdate(deleteData);
+
+                    String checkData = "SELECT studentNum FROM student_grade "
+                            + "WHERE studentNum = '" + addStudents_studentNum.getText() + "'";
+
+                    prepare = connect.prepareStatement(checkData);
+                    result = prepare.executeQuery();
+
+                    if (result.next()) {
+                        String deleteGrade = "DELETE FROM student_grade WHERE "
+                                + "studentNum = '" + addStudents_studentNum.getText() + "'";
+
+                        statement = connect.createStatement();
+                        statement.executeUpdate(deleteGrade);
+
+                    }
+
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Deleted!");
+                    alert.showAndWait();
+
+
+                    addStudentsShowListData();
+
+                    addStudentsClear();
+
+                } else {
+                    return;
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
+
+    public void addStudentsClear() {
+        addStudents_studentNum.setText("");
+        addStudents_year.getSelectionModel().clearSelection();
+        addStudents_course.getSelectionModel().clearSelection();
+        addStudents_firstName.setText("");
+        addStudents_lastName.setText("");
+        addStudents_gender.getSelectionModel().clearSelection();
+        addStudents_birth.setValue(null);
+        addStudents_status.getSelectionModel().clearSelection();
+        addStudents_imageView.setImage(null);
+
+        getData.path = "";
+    }
+
+    public void addStudentsInsertImage() {
+
+        FileChooser open = new FileChooser();
+        open.setTitle("Open Image File");
+        open.getExtensionFilters().add(new ExtensionFilter("Image File", "*jpg", "*png"));
+
+        File file = open.showOpenDialog(main_form.getScene().getWindow());
+
+        if (file != null) {
+
+            image = new Image(file.toURI().toString(), 120, 149, false, true);
+            addStudents_imageView.setImage(image);
+
+            getData.path = file.getAbsolutePath();
+
+        }
+    }
+
+    public void addStudentsSearch() {
+
+        FilteredList<studentData> filter = new FilteredList<>(addStudentsListD, e -> true);
+
+        addStudents_search.textProperty().addListener((Observable, oldValue, newValue) -> {
+
+            filter.setPredicate(predicateStudentData -> {
+
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                String searchKey = newValue.toLowerCase();
+
+                if (predicateStudentData.getStudentNum().toString().contains(searchKey)) {
+                    return true;
+                } else if (predicateStudentData.getYear().toLowerCase().contains(searchKey)) {
+                    return true;
+                } else if (predicateStudentData.getCourse().toLowerCase().contains(searchKey)) {
+                    return true;
+                } else if (predicateStudentData.getFirstName().toLowerCase().contains(searchKey)) {
+                    return true;
+                } else if (predicateStudentData.getLastName().toLowerCase().contains(searchKey)) {
+                    return true;
+                } else if (predicateStudentData.getGender().toLowerCase().contains(searchKey)) {
+                    return true;
+                } else if (predicateStudentData.getBirth().toString().contains(searchKey)) {
+                    return true;
+                } else if (predicateStudentData.getStatus().toLowerCase().contains(searchKey)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+        });
+
+        SortedList<studentData> sortList = new SortedList<>(filter);
+
+        sortList.comparatorProperty().bind(addStudents_tableView.comparatorProperty());
+        addStudents_tableView.setItems(sortList);
+
+    }
+
+    private String[] yearList = {"First Year", "Second Year", "Third Year", "Fourth Year"};
+
+    public void addStudentsYearList() {
+
+        List<String> yearL = new ArrayList<>();
+
+        for (String data : yearList) {
+            yearL.add(data);
+        }
+
+        ObservableList ObList = FXCollections.observableArrayList(yearL);
+        addStudents_year.setItems(ObList);
+
+    }
+
+    public void addStudentsCourseList() {
+
+        String listCourse = "SELECT * FROM course";
+
+        connect = database.connectDb();
+
+        try {
+
+            ObservableList listC = FXCollections.observableArrayList();
+
+            prepare = connect.prepareStatement(listCourse);
+            result = prepare.executeQuery();
+
+            while (result.next()) {
+                listC.add(result.getString("course"));
+            }
+            addStudents_course.setItems(listC);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private String[] genderList = {"Male", "Female", "Others"};
+
+    public void addStudentsGenderList() {
+        List<String> genderL = new ArrayList<>();
+
+        for (String data : genderList) {
+            genderL.add(data);
+        }
+
+        ObservableList ObList = FXCollections.observableArrayList(genderL);
+        addStudents_gender.setItems(ObList);
+    }
+
+    private String[] statusList = {"Enrolled", "Not Enrolled", "Inactive"};
+
+    public void addStudentsStatusList() {
+        List<String> statusL = new ArrayList<>();
+
+        for (String data : statusList) {
+            statusL.add(data);
+        }
+
+        ObservableList ObList = FXCollections.observableArrayList(statusL);
+        addStudents_status.setItems(ObList);
+    }
+
+    public ObservableList<studentData> addStudentsListData() {
+
+        ObservableList<studentData> listStudents = FXCollections.observableArrayList();
+
+        String sql = "SELECT * FROM student";
+
+        connect = database.connectDb();
+
+        try {
+            studentData studentD;
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            while (result.next()) {
+                studentD = new studentData(result.getInt("studentNum"),
+                        result.getString("year"),
+                        result.getString("course"),
+                        result.getString("firstName"),
+                        result.getString("lastName"),
+                        result.getString("gender"),
+                        result.getDate("birth"),
+                        result.getString("status"),
+                        result.getString("image"));
+
+                listStudents.add(studentD);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listStudents;
+    }
+
+    private ObservableList<studentData> addStudentsListD;
+
+    public void addStudentsShowListData() {
+        addStudentsListD = addStudentsListData();
+
+        addStudents_col_studentNum.setCellValueFactory(new PropertyValueFactory<>("studentNum"));
+        addStudents_col_year.setCellValueFactory(new PropertyValueFactory<>("year"));
+        addStudents_col_course.setCellValueFactory(new PropertyValueFactory<>("course"));
+        addStudents_col_firstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        addStudents_col_lastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        addStudents_col_gender.setCellValueFactory(new PropertyValueFactory<>("gender"));
+        addStudents_col_birth.setCellValueFactory(new PropertyValueFactory<>("birth"));
+        addStudents_col_status.setCellValueFactory(new PropertyValueFactory<>("status"));
+
+        addStudents_tableView.setItems(addStudentsListD);
+
+    }
+
+    public void addStudentsSelect() {
+
+        studentData studentD = addStudents_tableView.getSelectionModel().getSelectedItem();
+        int num = addStudents_tableView.getSelectionModel().getSelectedIndex();
+
+        if ((num - 1) < -1) {
+            return;
+        }
+
+        addStudents_studentNum.setText(String.valueOf(studentD.getStudentNum()));
+        addStudents_firstName.setText(studentD.getFirstName());
+        addStudents_lastName.setText(studentD.getLastName());
+        addStudents_birth.setValue(LocalDate.parse(String.valueOf(studentD.getBirth())));
+
+        String uri = "file:" + studentD.getImage();
+
+        image = new Image(uri, 120, 149, false, true);
+        addStudents_imageView.setImage(image);
+
+        getData.path = studentD.getImage();
+
+    }
+
+
 }
