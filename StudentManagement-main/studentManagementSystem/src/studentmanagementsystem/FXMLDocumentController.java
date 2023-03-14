63c91b39f90bae36513d9f8e44d21c91e211f48e
Charmzz <118677880+Tepinsim@users.noreply.github.com>
2023-03-14 15:41:24 +0700

    Update FXMLDocumentController.java

--- StudentManagement-main/studentManagementSystem/src/studentmanagementsystem/FXMLDocumentController.java
+++ StudentManagement-main/studentManagementSystem/src/studentmanagementsystem/FXMLDocumentController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package studentmanagementsystem;
 
 import java.net.URL;
@@ -43,8 +38,7 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private Button close;
-    
-//    DATABASE TOOls
+
     private Connection connect;
     private PreparedStatement prepare;
     private ResultSet result;
