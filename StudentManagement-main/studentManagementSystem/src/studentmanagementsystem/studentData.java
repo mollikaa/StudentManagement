e197eab3ea3adbf4958e5e928a739d3855aa806c
Charmzz <118677880+Tepinsim@users.noreply.github.com>
2023-03-14 15:40:29 +0700

    Update studentData.java

--- StudentManagement-main/studentManagementSystem/src/studentmanagementsystem/studentData.java
+++ StudentManagement-main/studentManagementSystem/src/studentmanagementsystem/studentData.java
@@ -18,7 +18,6 @@ public class studentData {
     private Double secondSem;
     private Double finals;
 
-    // MAKE SURE THAT SAME DATATYPE THAT YOU WILL PUT THERE 
     public studentData(Integer studentNum, String year, String course, String firstName, String lastName, String gender, Date birth, String status, String image) {
         this.studentNum = studentNum;
         this.year = year;
@@ -87,6 +86,4 @@ public class studentData {
     public Double getFinals() {
         return finals;
     }
-
-    // PLEASE FOLLOW ME IF THE VARS ARE THE SAME TO ME 
 }
