cb7e6f2baf488ce3e92172230797c4139fac172a
Tepinsim <thydateppin@yahoo.com>
2023-03-16 15:44:50 +0700

    dashboard

--- StudentManagement-main/studentManagementSystem/src/studentmanagementsystem/studentData.java
+++ StudentManagement-main/studentManagementSystem/src/studentmanagementsystem/studentData.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package studentmanagementsystem;
 
 import java.sql.Date;
@@ -23,7 +19,6 @@ public class studentData {
     private Double secondSem;
     private Double finals;
 
-    // MAKE SURE THAT SAME DATATYPE THAT YOU WILL PUT THERE 
     public studentData(Integer studentNum, String year, String course, String firstName, String lastName, String gender, Date birth, String status, String image) {
         this.studentNum = studentNum;
         this.year = year;
@@ -93,5 +88,5 @@ public class studentData {
         return finals;
     }
 
-    // PLEASE FOLLOW ME IF THE VARS ARE THE SAME TO ME 
+
 }
